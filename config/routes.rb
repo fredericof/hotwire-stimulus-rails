9a16ad54350b24ecab529b4f880e51fc15013ea2
Frederico Bitencourt <frederico@leadsoft.inf.br>
2022-03-31 19:29:06 -0300

    Added pagination widget

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   root "quotes#index"
-
+  
   resources :quotes
+  resources :widgets
   get '/search_index', to: 'quotes#search_index', as: 'search_index'
   get '/slider_index', to: 'quotes#slider_index', as: 'slider_index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_30_112337) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_31_174308) do
   create_table "quotes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "widgets", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
